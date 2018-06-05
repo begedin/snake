3c2e72291aaaaec53fb5dfbc02a7fe03d688f9eb
Nikola Begedin <begedinnikola@gmail.com>
2018-06-05 20:59:50 +0200

    Basic game loop

--- snake.js
+++ snake.js
@@ -1,2 +1,24 @@
+class Game {
+  constructor() {
+    const canvas = document.getElementById('snake');
+    const context = canvas.getContext('2d');
+
+    setInterval(() => {
+      this.update();
+      this.draw(context);
+    }, 120);
+  }
+
+  draw(context) {
+    context.fillStyle = 'black';
+    context.fillRect(0, 0, context.canvas.width, context.canvas.height);
+  }
+
+  update() {
+
+  }
+}
+
 window.onload = function() {
+  window.game = new Game();
 }
